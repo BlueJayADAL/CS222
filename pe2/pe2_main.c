20054f5699d0e657f8b0d4e69cf81bf3e3750cfb
Peilong <long880420@gmail.com>
2023-02-12 16:40:27 -0500

    Add pe2 main file

---
+++ pe2/pe2_main.c
@@ -0,0 +1,26 @@
+/* 
+ * File: pe2_main.c
+ * Author: Dr. Li
+ * Date: 02/18/2022
+ * Description: Source file for main function of PE2
+ * Copyright © 2022 Etown. All rights reserved.
+ */
+
+
+// Preprocessor directives
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main() {
+    char cmd;		// Input command
+    int n;			// Integer used in calculations
+    char junk;		// Used to clear line
+    int nVals;		// # values read
+    int result;     // Result for the calculations
+
+    // COMPLETE THE REST OF THE MAIN FUNCTION
+    
+
+    return 0;
+}
