12365f25b12246d9cbebd27a9b8d6f78325b6981
Peilong <long880420@gmail.com>
2023-02-12 16:41:39 -0500

    add pe2 functions and header

---
+++ pe2/pe2_functions.c
@@ -0,0 +1,33 @@
+/* 
+ * File: pe2_functions.c
+ * Author: Dr. Li
+ * Date: 02/18/2022
+ * Description: Source file for function definitions
+ */
+
+// Preprocessor directives
+#include "pe2_functions.h"
+
+/* 
+ * Name: factorial()
+ * Arguments: num - integer 
+ * Return: integer
+ * Description: Function takes in an integer number num,
+ *              and returns the factorial of num!
+ */
+int factorial(int num) {
+    // CHANGE THE NEXT LINE WITH YOUR OWN CODE
+    return 0;
+}
+
+/* 
+ * Name: power()
+ * Arguments: num - int
+ * Return: int
+ * Description: Function takes in an integer number num,
+ *              and returns 2 raise to the power of num
+ */
+int power(int num) {
+    // CHANGE THE NEXT LINE WITH YOUR OWN CODE
+    return 0;
+}
